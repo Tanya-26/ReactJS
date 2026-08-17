4920302ba30f593437ac01d6b4403a47f249a8d7
Tanya-26 <tamvimaheshwari26@gmail.com>
2026-08-17 12:55:58 +0530

    made my first object

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,16 @@
-function App(){
-  return <h1>Welcome to ReactJS</h1>
+function App() {
+  let listinfo = {
+    id: 1,
+    name: "Raman",
+    class: "ReactJS",
+  };
+  return (
+    <div>
+      <h1>Welcome to ReactJS</h1>
+      <h1>Id: {listinfo.id}</h1>
+      <h1>Name: {listinfo.name}</h1>
+      <h1>Class: {listinfo.class}</h1>
+    </div>
+  );
 }
 export default App;
