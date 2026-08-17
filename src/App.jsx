ee9a266ada39679d95ea7c71135675ac699f72b8
Tanya-26 <tamvimaheshwari26@gmail.com>
2026-08-17 13:09:45 +0530

    made my first array

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,22 @@ function App() {
     name: "Raman",
     class: "ReactJS",
   };
+  let language = ["Java"," Python", " JavaScript", " Mojo", " C++"];
   return (
     <div>
       <h1>Welcome to ReactJS</h1>
       <h1>Id: {listinfo.id}</h1>
       <h1>Name: {listinfo.name}</h1>
       <h1>Class: {listinfo.class}</h1>
+      <h2>Welcome to list of languages</h2>
+      <h1>{language}</h1>
+      <ul>
+        {language.map((l,index) => (
+          <li key={index}>
+            <h4>{l}</h4>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
